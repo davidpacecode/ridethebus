2f67f231a6ba9bda22435a0b5cb5a05fbd2fa314
davidpacecode <davidpace@gmail.com>
2025-07-18 18:08:50 -0500

    have basic auth started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "pages#home"
   get "about", to: "pages#about"  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
